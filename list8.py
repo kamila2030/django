c55c0465bce215e2fd5a795c5dcc3dc6f136f7df
KAMILA <kamilausubalieva3@gmail.com>
2026-07-02 11:24:49 +0300

    Add files via upload

---
+++ list8.py
@@ -0,0 +1,19 @@
+def loginf(request): 
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Вы вошли как {username}')
+                return redirect('/profile/')
+            else:
+                messages.error(request, 'Неправильное имя пользователя или пароль')
+        else:
+            messages.error(request, 'Неправильное имя пользователя или пароль')
+    
+    form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
